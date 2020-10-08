a465a280002b18e23ec284d94728136c53e3564e
Jiyoon Park <jiyoonpark0207@gmail.com>
2020-10-08 23:48:16 +0900

    Question 10 complete!

--- src/Q2018/Question10.java
+++ src/Q2018/Question10.java
@@ -1,3 +1,5 @@
+package Q2018;
+
 import java.util.Scanner;
 
 public class Question10 {
@@ -17,41 +19,54 @@ public class Question10 {
             relations[i][0]=scanner.nextInt();
             relations[i][1]=scanner.nextInt();
         }
-        int time =timeManagement(values, relations, size);
+        int goal = scanner.nextInt();
+
+        int time =timeManagement(values, relations, goal);
         System.out.println(time);
 
     }
     private static void solution(int [] values, int [][] relations){
      //   int total = timeManagement(values, relation, )
     }
-    private static int timeManagement(int [] values, int[][] relation, int cur){
+    private static int timeManagement(int [] values, int[][] relation, int goal){
+       // System.out.println("------------------------------------[ "+ goal + " ]------------------------------------------");
         int total=0;
         int [] t ;
         int j=0;
         int big=0;
         for(int i=0; i<relation.length;i++){
-            if(relation[i][1]==cur){
+            if(relation[i][1]==goal){
                 total +=1;
             }
         }
-        System.out.println(cur+"가 "+total+"개 있습니다.");
+        //System.out.println(goal+"가 "+total+"개 있습니다.");
         t= new int [total];
 
         for(int i=0; i<relation.length;i++){
-            if(cur==1) return values[0];
-            if(relation[i][1]==cur){
-                t[j++] = timeManagement(values, relation, cur-1);
-                System.out.println(t[j-1]);
+            if(goal==1) {
+        //        System.out.println("1번쨰이므로 바로 return");
+                return values[0];
+            }
+            if(relation[i][1]==goal){
+                t[j++] = timeManagement(values, relation, relation[i][0]);
+        //        System.out.println(goal+"번째 :"+t[j-1]);
             }
         }
         big = t[0];
-        System.out.print("돌아온 값은 : ");
+       // System.out.print(goal + "의 돌아온 값은 : ");
         for(int i=0; i<total ; i++) {
-            System.out.print(t[i]+" ");
+        //    System.out.print(t[i]+" ");
             if (big < t[i]) {
                 big = t[i];
             }
         }
-        return big+values[cur-1];
+        //System.out.println(goal+"에서 가장 큰 값은 "+big );
+        return big+values[goal-1];
     }
 }
+/*4 4
+10 20 100 20
+1 2
+1 3
+2 4
+3 4*/
