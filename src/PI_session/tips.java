85b1869102b1dc1d052ae4cfb85e3a9b9dc1109c
Jiyoon Park <jiyoonpark0207@gmail.com>
2020-10-10 00:21:03 +0900

    updated tips

--- src/PI_session/tips.java
+++ src/PI_session/tips.java
@@ -20,7 +20,20 @@ public class tips {
         for(int i:array){
             System.out.print(i+" ");
         }
+        int arr1[] = new int[10];
+        Arrays.fill(arr1, -1);
+        for(int i=0; i<10; i++){
+                System.out.print(arr1[i]+" ");
+        }System.out.println();
         int len = array.length;
         int len_Str = b.length();
-    }
+        int arr [][] = new int [10][10];
+        Arrays.fill(arr, -1);
+        for(int i=0; i<10; i++){
+            for(int j=0; j<10; j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+        }
 }
