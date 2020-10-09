48055b091bfa280577c6c72a2ff62f277bc260b0
Jiyoon Park <jiyoonpark0207@gmail.com>
2020-10-10 00:06:07 +0900

    2019/9 incomplete

--- src/Q2019/Question9.java
+++ src/Q2019/Question9.java
@@ -1,5 +1,6 @@
 package Q2019;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Question9 {
@@ -7,9 +8,9 @@ public class Question9 {
         Scanner scanner = new Scanner(System.in);
         int size = scanner.nextInt();
         int array [][] = new int [size][size];
-        int j=0;
+        //Arrays.fill(array, -1);
         for(int i=0; i<size;i++){
-            for(int j=0; j<i;j++){
+            for(int j=0; j<i+1;j++){
                 array[i][j] = scanner.nextInt();
             }
         }
@@ -18,12 +19,27 @@ public class Question9 {
     }
 
     private static void soultion(int[][] array, int size) {
-        nextBig(array, size, 1);
+        int total = nextBig(array, size, 0, 0, 0);
+        System.out.println(total);
     }
 
-    private static int nextBig(int array[][] ,int size, int i) {
-        int max = array[i][0];
-
-        if( i==size-1) return max;
+    private static int nextBig(int array[][] ,int size, int i, int j, int total) {
+        int a = array[i+1][j];
+        int b = array[i+1][j+1];
+        int c;
+        int max;
+        if( a>b) {
+            max =a;
+            c=j;
+        }
+        else {
+            max =b;
+            c=j+1;
+        }
+        total+=max;
+        if( i==size-1) return max+total;
+        else{
+            return nextBig(array, size, i+1, c, total);
+        }
     }
 }
